d9bfc1c6a19c0c05eb1781c95fdd2a9582c52e55
Lloyd <lloyd@dreamatic.digital>
2020-03-28 21:38:57 +1100

    added hero unit

--- functions.php
+++ functions.php
@@ -7,16 +7,17 @@ define( 'DR_DEV_MODE', true );
 
 
 //load shortcodes from individual files
+require_once(__DIR__ . '/components/nu_hero.php');
 
 //register and enqueue additional styles and scripts
 
-function dr_enqueue(){
+function nu_enqueue(){
   
   $uri = get_theme_file_uri();
   $ver = DR_DEV_MODE ? time() : false;
 
-  wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css', [], $ver );
-  wp_enqueue_style( 'dr_styles' );
+  wp_register_style( 'nu_styles', $uri . '/css/nutricia.css', [], $ver );
+  wp_enqueue_style( 'nu_styles' );
 }
 
-add_action( 'wp_enqueue_scripts', 'dr_enqueue' );
+add_action( 'wp_enqueue_scripts', 'nu_enqueue' );

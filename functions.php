f46a2be1d341b4dd28f175d17adef25a80b9aedb
Lloyd <lloyd@dreamatic.digital>
2020-03-29 15:47:01 +1100

    added formats

--- functions.php
+++ functions.php
@@ -22,3 +22,31 @@ function nu_enqueue(){
 }
 
 add_action( 'wp_enqueue_scripts', 'nu_enqueue' );
+
+//add custom formats to editor
+
+function wpb_mce_buttons_2($buttons) {
+  array_unshift($buttons, 'styleselect');
+  return $buttons;
+}
+add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
+
+function my_mce_before_init_insert_formats( $init_array ) {  
+    
+      $style_formats = array(  
+
+        array(  
+          'title' => 'White Text',  
+          'block' => 'span',  
+          'classes' => 'nu-white',
+          'wrapper' => true, 
+      ),  
+      
+  );
+
+  $init_array['style_formats'] = json_encode( $style_formats );  
+   
+  return $init_array;  
+
+} 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );

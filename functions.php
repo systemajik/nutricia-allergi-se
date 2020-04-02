a6e940c06a6d17b9fde094aa4d8f45a28a1081b7
Lloyd <lloyd@dreamatic.digital>
2020-04-02 23:56:25 +1100

    CSS

--- functions.php
+++ functions.php
@@ -24,3 +24,49 @@ function nu_enqueue(){
 }
 
 add_action( 'wp_enqueue_scripts', 'nu_enqueue' );
+
+/*
+* Callback function to filter the MCE settings
+*/
+ 
+function my_mce_before_init_insert_formats( $init_array ) {  
+ 
+  // Define the style_formats array
+   
+      $style_formats = array(  
+  /*
+  * Each array child is a format with it's own settings
+  * Notice that each array has title, block, classes, and wrapper arguments
+  * Title is the label which will be visible in Formats menu
+  * Block defines whether it is a span, div, selector, or inline style
+  * Classes allows you to define CSS classes
+  * Wrapper whether or not to add a new block-level element around any selected elements
+  */
+          array(  
+              'title' => 'Content Block',  
+              'block' => 'span',  
+              'classes' => 'content-block',
+              'wrapper' => true,
+               
+          ),  
+          array(  
+              'title' => 'Blue Button',  
+              'block' => 'span',  
+              'classes' => 'blue-button',
+              'wrapper' => true,
+          ),
+          array(  
+              'title' => 'Red Button',  
+              'block' => 'span',  
+              'classes' => 'red-button',
+              'wrapper' => true,
+          ),
+      );  
+      // Insert the array, JSON ENCODED, into 'style_formats'
+      $init_array['style_formats'] = json_encode( $style_formats );  
+       
+      return $init_array;  
+     
+  } 
+  // Attach callback to 'tiny_mce_before_init' 
+  add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' ); 
